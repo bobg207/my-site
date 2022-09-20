58fa09909a18a56f83c5448530403b095bb223c8
Robert Girsch <51169804+bobg207@users.noreply.github.com>
2022-09-20 22:39:14 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,19 @@
+const burgerBtn = document.querySelector('#burger-btn');
+const barsIcon = document.querySelector('#menu-open');
+const xmarkIcon = document.querySelector('#menu-closed');
+const navMenu = document.querySelector('.link-wrapper');
+let isOpen = false;
+
+burgerBtn.addEventListener("click", () => {
+  if (isOpen) {
+    navMenu.style.display = "none";
+    xmarkIcon.style.display = "none";
+    barsIcon.style.display = "block";
+    isOpen = false;
+  } else {
+    navMenu.style.display = "flex";
+    barsIcon.style.display = "none";
+    xmarkIcon.style.display = "block";
+    isOpen = true;
+  }  
+});
