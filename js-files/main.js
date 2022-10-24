397a0bd6786f054e34515047ac406cca2f71b705
Robert Girsch <51169804+bobg207@users.noreply.github.com>
2022-10-24 17:36:44 +0000

    filled sailing page with content

--- js-files/main.js
+++ js-files/main.js
@@ -2,8 +2,29 @@ const burgerBtn = document.querySelector('#burger-btn');
 const xmarkIcon = document.querySelector('#menu-open');
 const barsIcon = document.querySelector('#menu-closed');
 const navMenu = document.querySelector('.link-wrapper');
-const navLinks = document.querySelectorAll('.link')
+const navLinks = document.querySelectorAll('.link');
+const links = document.querySelectorAll(".link");
+const linkMenu = document.querySelector(".link-wrapper");
+
 let isOpen = false;
+
+window.addEventListener('load', (event) => {
+  const currentPage = localStorage.getItem("page");
+  alert(currentPage);
+  links.forEach((link, index, objs) => {
+    
+    // if (!currentPage) {
+    //   currentPage = "Home"
+    // }
+    
+    page = link.textContent;
+    
+    if (page === currentPage) {
+      link.style.textDecoration = 'underline'}
+    else {
+      link.style.textDecoration = 'none'};
+  });
+})
 
 burgerBtn.addEventListener("click", () => {
   if (isOpen) {
@@ -18,3 +39,8 @@ burgerBtn.addEventListener("click", () => {
     isOpen = true;
   }  
 });
+
+function getChosenLink(anchor) {
+  localStorage.setItem("page", anchor.textContent)
+  alert(localStorage.getItem("page"))
+}
