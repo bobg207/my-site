16b14ce74bc40588c5b331a74d0f542f47d8ee16
Robert Girsch <51169804+bobg207@users.noreply.github.com>
2022-11-03 01:21:29 +0000

    fixed links href's

--- js-files/main.js
+++ js-files/main.js
@@ -2,26 +2,14 @@ const burgerBtn = document.querySelector('#burger-btn');
 const xmarkIcon = document.querySelector('#menu-open');
 const barsIcon = document.querySelector('#menu-closed');
 const navMenu = document.querySelector('.link-wrapper');
-const navLinks = document.querySelectorAll('.link');
-const links = document.querySelectorAll(".link");
-const linkMenu = document.querySelector(".link-wrapper");
+// const navLinks = document.querySelectorAll('.link');
+// const links = document.querySelectorAll(".link");
+// const linkMenu = document.querySelector(".link-wrapper");
 
 let isOpen = false;
 
-(function() {
-  const url = window.location.href;
-  (".link-wrapper a").forEach(function() {
-
-    if (url == this.href) {
-      alert(this.href)
-    //   link.style.textDecoration = 'underline'}
-    // else {
-    //   link.style.textDecoration = 'none'};
-    }
-  });
-})
-
 burgerBtn.addEventListener("click", () => {
+  // alert('clicked')
   if (isOpen) {
     navMenu.style.display = "none";
     xmarkIcon.style.display = "none";
